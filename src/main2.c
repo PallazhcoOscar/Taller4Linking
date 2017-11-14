c6a03d9391921fe4cbff8816337618c99e7f997f
oscarP <oepallaz@espol.edu.ec>
2017-11-14 11:32:00 -0500

    Modificación del main2 para que solicite por teclado los valores de los vectores v1 y v2

--- src/main2.c
+++ src/main2.c
@@ -3,20 +3,37 @@
 #include <stdio.h>
 #include "vector.h"
 
-Vector v1 = {1, 2};
-Vector v2 = {3, 4};
+/*Crear los vectores*/
+Vector v1;
+Vector v2;
 Vector v3;
 Vector v4;
-int main()
-{
-	printf("v1 = [%d %d]\n", v1.x, v1.y);
-	printf("v2 = [%d %d]\n", v2.x, v2.y);
+int main(){
+	/*Crear los vectores*/
+	Vector v1, v2, v3, v4;
+
+	/*Pedir y asignar los valores de los vectores v1 y v2*/
+	printf("Ingrese la coordenada en x del vector v1: ");
+	scanf("%d", &v1.x);
+	printf("Ingrese la coordenada en y del vector v1: ");
+	scanf("%d", &v1.y);
+	printf("Ingrese la coordenada en x del vector v2: ");
+	scanf("%d", &v2.x);
+	printf("Ingrese la coordenada en y del vector v2: ");
+	scanf("%d", &v2.y);
+
+	printf("\nLos vectores ingresados son: \n");
+	printf("\tv1 = [%d %d]\n", v1.x, v1.y);
+	printf("\tv2 = [%d %d]\n", v2.x, v2.y);
+
+	/*uso de las funciones*/
 	addvec(&v1, &v2, &v3);
 	multvec(&v1, &v2, &v4);
-	printf("Uso de la fuunción addvec:\n");
-	printf("v3 =[%d %d]\n", v3.x, v3.y);
-	printf("\n");
-	printf("Uso de la función multvec: \n");
-	printf("v4 = [%d %d]\n", v4.x, v4.y);
+	printf("\nUso de la función addvec:\n");
+	printf("\tv3 = [%d %d]\n", v3.x, v3.y);
+
+	printf("\nUso de la función multvec: \n");
+	printf("\tv4 = [%d %d]\n", v4.x, v4.y);
+
 	return 0;
 }
