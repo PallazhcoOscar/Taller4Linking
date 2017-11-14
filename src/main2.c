c38dded193084992b35e167e156e4f93438c5ebf
oscarP <oepallaz@espol.edu.ec>
2017-11-14 11:03:35 -0500

    Actualización del main2 para que utilize el tipo de dato Vector

--- src/main2.c
+++ src/main2.c
@@ -3,15 +3,20 @@
 #include <stdio.h>
 #include "vector.h"
 
-int x[2] = {1, 2};
-int y[2] = {3, 4};
-int z[2];
-int k[2];
+Vector v1 = {1, 2};
+Vector v2 = {3, 4};
+Vector v3;
+Vector v4;
 int main()
 {
-	addvec(x, y, z, 2);
-	multvec(x, y, k, 2);
-	printf("z = [%d %d]\n", z[0], z[1]);
-	printf("k = [%d %d]\n", k[0], k[1]);
+	printf("v1 = [%d %d]\n", v1.x, v1.y);
+	printf("v2 = [%d %d]\n", v2.x, v2.y);
+	addvec(&v1, &v2, &v3);
+	multvec(&v1, &v2, &v4);
+	printf("Uso de la fuunción addvec:\n");
+	printf("v3 =[%d %d]\n", v3.x, v3.y);
+	printf("\n");
+	printf("Uso de la función multvec: \n");
+	printf("v4 = [%d %d]\n", v4.x, v4.y);
 	return 0;
 }
